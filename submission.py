9ff321dab09d8bb547b979d0652b28f21730ee4a
Aditi Jaydeep Shelke <aditishelke333@gmail.com>
2025-05-05 01:52:35 +0000

    Implemented liar puzzle logic and completed Problem 3 as per instructions

--- submission.py
+++ submission.py
@@ -1,5 +1,15 @@
 import collections, sys, os
+
 from logic import *
+def ExactlyOne(lst):
+    at_least_one = OrList(lst)
+    at_most_one = AndList([
+        Not(And(x, y))
+        for i, x in enumerate(lst)
+        for j, y in enumerate(lst)
+        if i < j
+    ])
+    return And(at_least_one, at_most_one)
 
 ############################################################
 # Problem 1: propositional logic
@@ -12,7 +22,8 @@ def formula1a():
     TV = Atom('TV')                 # whether I'm watching TV
     productive = Atom('Productive')               # whether I'm being productive
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return Implies(And(tomorrow, TV), Not(productive))
+    # raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 # sentence: Either I'll go to the gym or go for a run (but not both).
@@ -21,7 +32,7 @@ def formula1b():
     gym = Atom('Gym')     # whether I'll go to the gym
     run = Atom('Run') # whether I'll go for a run
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return And(Or(gym, run), Not(And(gym, run)))
     # END_YOUR_CODE
 
 # sentence: The store is open if and only if the sign says "open" and the lights are on.
@@ -31,7 +42,9 @@ def formula1c():
     O = Atom('Open')                # whether the store is open
     lights = Atom('Lights')  # whether the lights are on
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    # raise Exception("Not implemented yet")
+    return Equiv(O, And(store, lights))
+
     # END_YOUR_CODE
 
 ############################################################
@@ -45,7 +58,18 @@ def formula2a():
     def Teacher(x): return Atom('Teacher', x)
     def Teaches(x, y): return Atom('Teaches', x, y)
     # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
+    x = Variable('$x')
+    y = Variable('$y')
+    z = Variable('$z')
+    return AndList([
+        Exists('$x', Student(x)),
+        Exists('$y', Teacher(y)),
+        Forall('$x', Or(Student(x), Teacher(x))),
+        Forall('$x', Implies(Student(x), Forall('$y', Not(Teaches(x, y))))),
+        Forall('$x', Implies(Teacher(x), Forall('$y', Not(Teaches(y, x))))),
+        Forall('$x', Implies(Student(x), Exists('$y', And(Teacher(y), Teaches(y, x)))))
+    ])
+    # raise Exception("Not implemented yet")
 
 # sentence: Teacher can also learn from teacher, but student cannot teach
 def formula2b():
@@ -54,7 +78,14 @@ def formula2b():
     def Teacher(x): return Atom('Teacher', x)
     def Teaches(x, y): return Atom('Teaches', x, y)
     # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
+    x = Variable('$x')
+    y = Variable('$y')
+    return AndList([
+        Forall('$x', Implies(Teacher(x), Exists('$y', And(Teacher(y), Teaches(y, x))))),
+        Forall('$x', Implies(Student(x), Forall('$y', Not(Teaches(x, y)))))
+    ])
+    # raise Exception("Not implemented yet")
+
 
 
 ############################################################
@@ -82,8 +113,25 @@ def liar():
     formulas = []
     # We provide the formula for fact 0 here.
     formulas.append(Equiv(TellTruth(adam), WearsRed(adam)))
-    # You should add 5 formulas, one for each of facts 1-5.
     # BEGIN_YOUR_CODE 
+    # You should add 5 formulas, one for each of facts 1-5.
+    formulas.append(Equiv(TellTruth(levi), WearsRed(adam)))
+    formulas.append(Equiv(TellTruth(john), WearsRed(levi)))
+    formulas.append(Equiv(TellTruth(luke), Not(WearsRed(john))))
+    # 5. Exactly one person is telling the truth
+    formulas.append(ExactlyOne([
+        TellTruth(adam),
+        TellTruth(levi),
+        TellTruth(john),
+        TellTruth(luke)
+    ]))
+    # 6. Exactly one person is wearing red
+    formulas.append(ExactlyOne([
+        WearsRed(adam),
+        WearsRed(levi),
+        WearsRed(john),
+        WearsRed(luke)
+    ]))
     
     # END_YOUR_CODE
     query = TellTruth('$x')
